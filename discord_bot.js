9a6e821d36e7a1950920f8b0669840ea136434b9
gen BTC <genBTC@hushmail.com>
2016-11-10 01:33:43 -0500

    commit bot from 12-3-15 until 10-31-16
    includes csapi image

--- discord_bot.js
+++ discord_bot.js
@@ -8,6 +8,9 @@ var youtube_plugin = new yt();
 var gi = require("./google_image_plugin");
 var google_image_plugin = new gi();
 
+var gi2 = require("./google_csapi_image_plugin");
+var google_image_plugin2 = new gi2();
+
 var wa = require("./wolfram_plugin");
 var wolfram_plugin = new wa();
 
@@ -28,7 +31,32 @@ var global_commandcount = {};
 var Discord = require("discord.js");
 var bot = new Discord.Client();
 
+//#dreamers "<#98469019455074304>"
 var commands = {
+    "airhorn": {
+        usage: "DUMMY COMMAND",
+        description: "ignore airhorn messages",
+        process: function(bot,msg,suffix){
+            null
+        }
+    },
+    "johncena": {
+        usage: "DUMMY COMMAND",
+        description: "ignore johncena messages",
+        process: function(bot,msg,suffix){
+            null
+        }
+    },    
+    "ss": {
+        usage: "<message>",
+        description: "bot says message",
+        process: function(bot,msg,suffix){ 
+            if (msg.author.username == 'genBTC'){
+                //bot.sendMessage(msg.channel.server.channels.get("name","dreamers"),suffix,true);
+                bot.sendMessage(msg.client.channels.get("name","general"),suffix,true);
+            }
+        }
+    },    
 	"gif": {
 		usage: "<image tags>",
 		description: "returns a random gif matching the tags passed",
@@ -50,6 +78,12 @@ var commands = {
 		process: function(bot,msg,suffix){
 			youtube_plugin.respond(suffix,msg.channel,bot);		   }
 	},
+	"yt": {
+		usage: "<video tags>",
+		description: "alias for youtube",
+		process: function(bot,msg,suffix){
+			youtube_plugin.respond(suffix,msg.channel,bot);		   }
+	},    
 	"sc": {
 		usage: "<google searches soundcloud>",
 		description: "gets first result off SC through google",
@@ -68,17 +102,29 @@ var commands = {
 		process: function(bot,msg,suffix){
 			google_plugin.respond(suffix,msg.channel,bot);		  }
 	},
-	"image": {
+	"image-oldapi": {
 		usage: "<image tags>",
 		description: "gets image matching tags from google",
 		process: function(bot,msg,suffix){ 
 			google_image_plugin.respond(suffix,msg.channel,bot);	}
 	},
+	"image": {
+		usage: "<image tags>",
+		description: "gets image matching tags from google",
+		process: function(bot,msg,suffix){ 
+			google_image_plugin2.respond(suffix,msg.channel,bot);	}
+	},
+    "img": {
+		usage: "<image tags>",
+		description: "alias for image",
+		process: function(bot,msg,suffix){ 
+			google_image_plugin2.respond(suffix,msg.channel,bot);	}
+	},
 	"meme": {
 		usage: 'memename "top text" "bottom text"',
 		description: "Generates a meme image (Quotes are required). For Help: type !meme to list available memes (long).",
 		process: function(bot,msg,suffix) {
-            //check if command was blank.
+            //check if command was blank. give help messages if blank.
 			if(!suffix) {
                 var i = 1;
 				bot.sendMessage(msg.author,"Currently available memes:");
@@ -101,7 +147,7 @@ var commands = {
                 bot.sendMessage(msg.author,'example: !meme Doge "Top-Text" "Bottom-Text"');                
                 return;
 			}
-			var tags = msg.content.split('"');
+			var tags = msg.content.toLowerCase().split('"');
 			var memetype = tags[0].split(" ")[1];
             var memeid = 0;
             Object.keys(memes).forEach(function(key) {
@@ -165,10 +211,10 @@ bot.on("ready", function () {
 	//loadFeeds();
     var str = "";
     for(var i=0;i<bot.channels.length;i++){
-        str += bot.channels[i].name + ", ";
+        str += "#" + bot.channels[i].name + ":" + bot.channels[i].id + ", ";
     }
-    console.log("Channels: " + str + "...);
-	console.log("Ready to begin! Serving in " + bot.channels.length + " channels");    
+    console.log("Channels: " + str + "...");
+	console.log("Ready! Begin serving in " + bot.channels.length + " channels...");    
 });
 
 bot.on("disconnected", function () {
@@ -179,10 +225,11 @@ bot.on("disconnected", function () {
 });
 
 //Log user status changes (online,idle,offline)
+/* 
 bot.on("presence", function(user,status,gameid) {
 	console.log(user.username+" went "+status);
 });
-/* 
+
 bot.on("warn", function(message) {
 	console.log("Warning: "+message);
 });
@@ -190,32 +237,54 @@ bot.on("debug", function(message) {
 	console.log("Debug: "+message);
 }); 
 */
+var triggerlists = [];
+/* old triggers
+    ["Izzoh","bruh","__***Disclaimer: WARNING, Brosplaining Detected! Not suitable for those with fuccboi allergies***__"],
+    ["doot doot","sexy","http://i.qkme.me/3q7nj6.jpg"],
+    ["doot doot","sick","https://dahliasent.files.wordpress.com/2015/07/mean-girls-i-cant-go-out-im-sick-meme-main.jpg"],
+    ["doot doot","asian","http://s2.quickmeme.com/img/04/0479b2bb2ff45d108f62efeff35edcc7afcb0e5e7bd58d320c855d4354e21277.jpg"],    
+        ["memoryruins","florida","http://images1.miaminewtimes.com/imager/a-florida-man-movie-exists-but-it/u/blog/6529585/floridamanmovie.jpg"],
+            ["rudedudeowns","laptop","http://i.imgflip.com/un3aj.jpg"],
+["","triggered","http://cdn.makeagif.com/media/8-17-2015/KAJCag.gif"],            
+
+
 var triggerlists = [
     ["","tranny","__***WARNING, possible Tranny detected! Not suitable for anyone else besides <@99676085565800448>. ***__"],
-    ["","triggered","http://i.imgur.com/jpjcLMr.png"],
-    ["TomSuns","dnb","__***DnB SPERG ALERT***__"],
-    ["Izzoh","bruh","__***Disclaimer: WARNING, Brosplaining Detected! Not suitable for those with fuccboi allergies***__"],
+    
+    ["","objection","http://vignette1.wikia.nocookie.net/sonicfanon/images/7/75/Objection.gif"],
+    ["TomSuns","potato","http://images.rapgenius.com/19e39361c54679cfa3fa205a1af4dbf2.616x446x1.png"],
+    ["Izzoh","proof","http://memeguy.com/photos/thumbs/i-have-to-point-this-out-way-too-often-around-here-22613.jpg"],
     ["GDIBass"," ex ","__**Ex talk Detected! Please seek help**__ @  __www.professional-counselling.com/getting-over-a-relationship.html__ "],
-    ["ungineer","idgaf","http://cdn.meme.am/instances/60732665.jpg"],
+    ["ungineer","idgaf","http://cdn.meme.am/instances/60732665.jpg & https://giphy.com/gifs/people-vtKkmj9I1DNK"],
     ["ungineer","dgaf","http://cdn.memegenerator.net/instances/53928498.jpg"],
     ["ungineer","give a fuck","https://memecrunch.com/meme/1PH5/don-t-give-a-fuck/image.jpg"],
     ["ungineer","triggered","http://cdn.meme.am/instances/61541029.jpg"],
-    ["rudedudeowns","laptop","http://i.imgflip.com/un3aj.jpg"],
+    ["ungineer","laid back","http://i.imgur.com/nYzYZIV.jpg"],
+    ["rudedudeowns","kys","http://i1.kym-cdn.com/entries/icons/original/000/011/113/killyourselves.png"],
     ["rudedudeowns","squat","https://41.media.tumblr.com/3cc74236150098d65afcb9b1e43d1f68/tumblr_nazuoyvoCF1qmbxsmo1_400.jpg"],
+    ["rudedudeowns","chicken","http://www.eattoperform.com/wp-content/uploads/2015/08/chicken-and-rice-meme.jpg"],
     ["dnytm","tinder","http://www.brobible.com/wp-content/uploads/2015/03/ap1wu.jpg"],
+    ["dnytm","sister","http://cdn.meme.am/instances/400x/53343409.jpg"],
     ["tehsma","reverb","http://s2.quickmeme.com/img/2b/2b787aea2af0566218a05925f148772288ea31e195394c4142b5631a7fb14b98.jpg"],
-    ["rld","viola","http://i.imgflip.com/una2l.jpg"]
+    ["rld","viola","http://i.imgflip.com/una2l.jpg"],
+    ["Fick","school","http://s2.quickmeme.com/img/b2/b23ca78926ee04d7be2a8ce0b3ccc08dffb9896245b0ffe346b744b473d87489.jpg"],
+    ["Fick","wifi","http://i.imgflip.com/unee0.jpg"],
+    ["scattrbrain","sancocho","http://images1.phoenixnewtimes.com/imager/u/original/6538999/mambossancocho.jpg"],
+    ["Oliver","snsd","http://memecrunch.com/meme/36NM6/you-should-be-studying-taeyeon/image.jpg"],
+    ["memoryruins",":^)","https://www.youtube.com/watch?v=FdghRwWfaOQ"]
+    
 ];
+*/
 
 bot.on("message", function (msg) {
-	msg.content = msg.content.toLowerCase();
+	//msg.content = msg.content.toLowerCase();
     //check if message is a command    
 	if(msg.author.id != bot.user.id && msg.content[0] === '!'){// || msg.content.indexOf(bot.user.mention()) == 0)){
 		console.log(msg.author.username + " " + msg.content);
-		var cmdTxt = msg.content.split(" ")[0].substring(1);
+		var cmdTxt = msg.content.split(" ")[0].substring(1).toLowerCase();
 		var suffix = msg.content.substring(cmdTxt.length+2);//add one for the ! and one for the space
 		if(msg.content.indexOf(bot.user.mention()) == 0){
-			cmdTxt = msg.content.split(" ")[1];
+			cmdTxt = msg.content.split(" ")[1].toLowerCase();
 			suffix = msg.content.substring(bot.user.mention().length+cmdTxt.length+2);
 		}
 		var cmd = commands[cmdTxt];
@@ -258,8 +327,10 @@ bot.on("message", function (msg) {
 			}
 			global_lastmsgnameandtime[author] = msg.timestamp; //always store the timestamp regardless .
 			//End Flood Protecton
-		} else
+        } else if (cmdTxt !== ""){
+            console.log(cmdTxt + ".");
 			bot.sendMessage(msg.channel, "Invalid command: " + cmdTxt);
+        }
 	} else {
 		//if message isn't a command or it is from bot, drop our own messages to prevent feedback loops
 		if(msg.author == bot.user){
@@ -268,8 +339,10 @@ bot.on("message", function (msg) {
         //TRIGGER WORD LIST PROCESSING.
         for(var i=0;i<triggerlists.length;i++){
             if(msg.author.username.indexOf(triggerlists[i][0]) > -1){
-                if(msg.content.indexOf(triggerlists[i][1]) > -1){
+                if(msg.content.toLowerCase().indexOf(triggerlists[i][1]) > -1){
                     bot.sendMessage(msg.channel,triggerlists[i][2]);
+                    console.log("Trigger:" + triggerlists[i][0] + ", " + triggerlists[i][1]);
+                    break;
                 }
             }
         }
